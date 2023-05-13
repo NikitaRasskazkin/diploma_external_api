1bf5bfb154f408611639284077ecb1c38dfc1c6b
NikitaRasskazkin <rasskazkin64@gmail.com>
2023-05-13 23:34:23 +0300

    feat: Added models ReportRecognition and ReportLog

--- report/models.py
+++ report/models.py
@@ -14,3 +14,21 @@ class Report(models.Model):
     text = models.TextField()
     status = models.CharField(max_length=3, choices=ReportStatus.choices)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_dttm = models.DateTimeField()
+    calculation_start_dttm = models.DateTimeField(null=True)
+    calculation_end_dttm = models.DateTimeField(null=True)
+    model_version = models.CharField(max_length=16, null=True)
+
+
+class ReportRecognition(models.Model):
+    id = models.AutoField
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    sentence = models.TextField()
+    is_paraphrase = models.BooleanField()
+    probability = models.FloatField()
+
+
+class ReportLog(models.Model):
+    id = models.AutoField
+    report = models.ForeignKey(Report, on_delete=models.CASCADE)
+    error = models.TextField()
